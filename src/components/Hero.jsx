ae4db195eb4a30cef80cf9538d46e7da4e1317a5
NetworkFlux <networkflux@proton.me>
2025-01-21 14:59:39 +0000

    add finished feature section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -133,7 +133,7 @@ const Hero = () => {
         </h1>
         <div className='absolute left-0 top-0 z-40 size-full'>
           <div className='mt-24 px-5 sm:px-10'>
-            <h1 className='special-font hero-heading text-blue-100'>redifi<b>n</b>e</h1>
+            <h1 className='special-font hero-heading text-blue-100'>redife<b>n</b>e</h1>
             <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>Enter the Metahame Layer <br/>Unleash the Play Economy</p>
             <Button id='watch-trailer' title='Watch Trailer' leftIcon={<TiLocationArrow />} containerClass='!bg-yellow-300 flex-center gap-1'/>
           </div>
